0140f68a83eec4a66f36904fc9de74bb43fc97b2
peaisge <raphi.lescot@hotmail.fr>
2017-03-19 16:47:21 +0100

    Affichage de la carte : à faire (deux essais)

--- js/plan.js
+++ js/plan.js
@@ -1,5 +1,11 @@
-var infowindow = new google.maps.InfoWindow({
-                                            content: "<b>centre de la carte</b>"
+var watchId;
+
+var infowindowStatique = new google.maps.InfoWindow({
+                                            content: "<b>Le lieu du mariage</b>"
+                                            });
+
+var infowindowMobile = new google.maps.InfoWindow({
+                                            content: "<b>Votre position</b>"
                                             });
 
 window.addEventListener('load', function(){
@@ -8,14 +14,43 @@ window.addEventListener('load', function(){
             
 $(window).on('hashchange', route);
 
-function afficheCarte(){
-    var mapId = navigator.geolocation.watchPosition(onSuccess, onError, {
+function afficheCarteStatique(){
+    var mapId = navigator.geolocation.getCurrentPosition(onSuccessStatique, onError, {
                                     maximumAge: 10000, timeout: 300000, enableHighAccuracy: true
                                 });
-    google.maps.event.addDomListener(window, 'load', onSuccess);
+    google.maps.event.addDomListener(window, 'load', onSuccessStatique); 
 }
 
-function onSuccess(position) {
+function afficheCarteMobile(){
+    watchId = navigator.geolocation.watchPosition(onSuccessMobile, onError, {
+                                    maximumAge: 10000, timeout: 300000, enableHighAccuracy: true
+                                });
+    google.maps.event.addDomListener(window, 'load', onSuccessMobile);
+}
+
+function arretGeolocalisation(){
+    navigator.geolocation.clearWatch(watchId);
+}
+
+function onSuccessStatique(){
+    var lat = 48.858565;
+    var lng = 2.347198;
+    
+    //Google Maps
+    var myLatlng = new google.maps.LatLng(lat,lng);
+    var mapOptions = {zoom: 16, center: myLatlng};
+    var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    var marker = new google.maps.Marker({
+                                        position: myLatlng,
+                                        map: map,
+                                        title : "Centre"
+                                        });
+    marker.addListener('click', function() {
+                       infowindowStatique.open(map, marker);
+                       });
+}
+
+function onSuccessMobile(position) {
     var lat = position.coords.latitude;
     var lng = position.coords.longitude;
     
@@ -29,73 +64,66 @@ function onSuccess(position) {
                                         title : "Centre"
                                         });
     marker.addListener('click', function() {
-                       infowindow.open(map, marker);
+                       infowindowMobile.open(map, marker);
                        });
-    $.get('functions.php', function(data){
-        
-    });
 }
 
 function onError(error) {
-    alert('code: ' + error.code + '\n' +
-          'message: ' + error.message + '\n');
+    switch(error.code){
+        case error.PERMISSION_DENIED:
+            alert("L'utilisateur n'a pas autorisé l'accès à sa position");
+            break;
+        case error.POSITION_UNAVAILABLE:
+            alert("L'emplacement de l'utilisateur n'a pas pu être déterminé");
+            break;
+        case error.TIMEOUT:
+            alert("Le service n'a pas répondu à temps");
+            break;
+    }
 }
 
 function route(){
     var page, hash = window.location.hash;
     switch(hash){
         case "#carte-site":
-            $.get('js/templates.html', function(templates){
-                window.onload = afficheCarte();
-                page = $(templates).filter('#tpl-carte-site').html();
-                $('#container').html(page);
-            }, 'html');
+            window.onload = afficheCarteStatique();
             break;
             
         case "#carte-geoloc":
-            $.get('js/templates.html', function(templates){
-                window.onload = afficheCarte();
-                page = $(templates).filter('#tpl-carte-geoloc').html();
-                $('#container').html(page);
-            }, 'html');
+            window.onload = afficheCarteMobile();
             break;
         
-        case "#reglages":
-            $('#map-canvas').html("");
-            $.get('js/templates.html', function(templates){
-                page = $(templates).filter('#tpl-reglages').html();
-                $('#container').html(page);
-            }, 'html');
+        case "#destroy-geoloc":
+            window.onload = arretGeolocalisation();
             break;
             
-        case "#profil":
-            $('#map-canvas').html("");
-            $.get('js/templates.html', function(templates){
-                page = $(templates).filter('#tpl-profil').html();
-                $('#container').html(page);
-            }, 'html');
-            break;
+        //case "#reglages":
+        //    $('#map-canvas').html("");
+        //    $.get('js/templates.html', function(templates){
+        //        page = $(templates).filter('#tpl-reglages').html();
+        //        $('#container').html(page);
+        //    }, 'html');
+        //    break;
             
-        case "#add-friends-event":
-            $.getJSON("http://www.enseignement.polytechnique.fr/informatique/INF471T/php/categoriesAchats.php", function(achats) {
-                $.get('js/templates.html', function(templates) {
-                    var template = $(templates).filter('#tpl-add-friends-event').html();
-                    page = Mustache.render(template, achats);
-                    document.getElementById("container").innerHTML = page;
-                }, 'html');
-            });
-            break;
+        //case "#profil":
+        //   $('#map-canvas').html("");
+        //    $.get('js/templates.html', function(templates){
+        //        page = $(templates).filter('#tpl-profil').html();
+        //        $('#container').html(page);
+        //    }, 'html');
+        //    break;
            
         default:
-            $.get('js/templates.html', function(templates){
-                window.onload = afficheCarte();
-                page = $(templates).filter('#tpl-carte-site').html();
-                $('#container').html(page);
-            }, 'html');
+            window.onload = afficheCarteStatique();
             break;
     }
 }
 route();
+
+jQuery("#mesboutons .btn").click(function(){
+        jQuery("#mesboutons .btn").removeClass('active');
+        jQuery(this).toggleClass('active'); 
+});
 
 
 
