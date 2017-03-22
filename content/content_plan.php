755e7ff3ddf1065fcef0917b9e8cfe3419109ce2
peaisge <raphi.lescot@hotmail.fr>
2017-03-23 00:47:46 +0100

    Affichage de la carte

--- content/content_plan.php
+++ content/content_plan.php
@@ -1,25 +1,17 @@
 <?php
-    session_start();
-    $API_key = AIzaSyAo2khzYz_-UH7axF6npJSeV6Elcv_nmN0;
+session_start();
+$API_key = "AIzaSyAo2khzYz_-UH7axF6npJSeV6Elcv_nmN0";
 ?>
 <!DOCTYPE html>
-
+<script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?sensor=true"></script>
 <div id="container">
     <br/>
-    <div class="btn-group btn-group-justified" id="mesboutons" role="group" aria-label="menu">
-        <div class="btn-group" role="group">
-            <a href="#carte-site">
-                <button type="button" class="btn btn-default active">Plan du site</button>
-            </a>
-        </div>
-        <div class="btn-group" role="group">
-            <a href="#carte-geoloc">
-                <button type="button" class="btn btn-default">Où je suis</button>
-            </a>
-        </div>
-    </div>
+    <b style="font-size: large">Le plan du site </b>
+    <button type="button" class="btn btn-default" aria-label="Left Align" onclick="geolocaliserMap()" style="float:right">
+        Ma position <i class="fa fa-location-arrow" aria-hidden="true"></i>
+    </button>
 </div>
+<br>
 <div id="map-canvas"></div>
-<script src="http://maps.google.com/maps/api/js?key=<?php echo $API_key ?>"></script>
-<script src="js/plan2.js"></script>
-
+<script src="js/plan.js"></script>
+<script async defer src="http://maps.google.com/maps/api/js?key=<?php echo $API_key?>&callback=afficheCarte"></script>
