560068e01869bf14a490df5ffee9bb3bf6a82fbd
peaisge <raphi.lescot@hotmail.fr>
2017-03-31 16:22:58 +0200

    Inachevé

--- utilities/changePassword.php
+++ utilities/changePassword.php
@@ -1,3 +1,18 @@
-<?php
+<!DOCTYPE html>
+<form action="" method="post">
+    <p>
+        <label for="ancienMdp">Actuel</label>
+        <input id="ancienMdp" type="password" required name="ancienMdp">
+    </p>
+    <p>
+        <label for="nouveauMdp">Nouveau</label>
+        <input id="nouveauMdp" type="password" required name="nouveauMdp">
+    </p>
+    <p>
+        <label for="nouveauMdp2">Confirmer</label>
+        <input id="nouveauMdp2" type="password" required name="nouveauMdp2">
+    </p>
+    <input type="submit" value="Enregistrer les modifications">
+</form>
 
 
