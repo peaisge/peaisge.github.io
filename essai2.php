6142c0cc4019bfdc17a5a81c35c0487ffe3b5809
peaisge <raphi.lescot@hotmail.fr>
2017-11-09 02:22:36 +0100

    Delete essai2.php

--- essai2.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$login = "admin";
-$password = "adminnon";
-require 'sql/database.php';
-require 'sql/user.php';
-$dbh = Database::connect();
-$user = User::getUser($dbh, $login);
-echo User::testPassword($user, $password);
